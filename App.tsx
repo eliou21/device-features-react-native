5aa078ad6034e6f1d0543ff4f03216d6dd256282
elisha <elisha_grace_ilao@dlsl.edu.ph>
2025-04-11 22:22:47 +0800

    made changes in app.tsx

--- App.tsx
+++ App.tsx
@@ -14,16 +14,24 @@ export default function App() {
 
   return (
     <ThemeProvider>
+
       <SavedPostsProvider>
+
         <LikedPostsProvider>
+
           <NavigationContainer>
+
             <Stack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false }}>
               <Stack.Screen name="Welcome" component={WelcomeScreen} />
               <Stack.Screen name="Main" component={BottomTabs} />
             </Stack.Navigator>
+
           </NavigationContainer>
+
         </LikedPostsProvider>
+
       </SavedPostsProvider>
+      
     </ThemeProvider>
   );
 }
